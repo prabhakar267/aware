a526640ad0f37966e0abe2c9d9b0320bfdcc961d
Andrew Marshall <amarshall@boomtownroi.com>
2017-10-11 11:09:09 -0400

    Optimized Imports.

--- Android/app/src/main/java/com/americanminion/Maps.java
+++ Android/app/src/main/java/com/americanminion/Maps.java
@@ -11,7 +11,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
